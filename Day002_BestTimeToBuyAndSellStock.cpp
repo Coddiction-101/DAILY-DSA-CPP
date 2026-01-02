1bb8f423025a6b93a05a8c107f97d36a0823248e
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-01-02 20:41:15 +0530

    Update Day002_BestTimeToBuyAndSellStock.cpp

--- Day002_BestTimeToBuyAndSellStock.cpp
+++ Day002_BestTimeToBuyAndSellStock.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
 
+int max_profit(vector<int> &prices)
+{
+    int min_price = INT_MAX;
+    int max_Profit = 0;
+
+    for (int price : prices)
+    {
+        if (price < min_price)
+            min_price = price;
+        else
+            max_Profit = max(max_Profit, price - min_price);
+    }
+    return max_Profit;
+}
+
+int main()
+{
+    vector<int> prices = {7, 1, 5, 3, 6, 4};
+    cout << "max profit: " << max_profit(prices);
+    return 0;
+}
