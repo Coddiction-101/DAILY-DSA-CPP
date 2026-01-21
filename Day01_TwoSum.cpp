bf9c7e0bdcc7918c68dbd2eb0b86f5e82686cae7
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-01-21 12:26:15 +0530

    Update Day01_TwoSum.cpp

--- Day01_TwoSum.cpp
+++ Day01_TwoSum.cpp
@@ -4,9 +4,7 @@
  * Difficulty: Easy
  * Topics: Array, Hash Table
  * Time Complexity: O(n)
- * Space Complexity: O(n)
- * Problem Link: https://leetcode.com/problems/two-sum/description/
- *
+ * Space Complexity: O(n) 
  * Problem Statement:
  * Given an array of integers nums and an integer target, return indices of the two numbers
  * such that they add up to target. You may assume that each input would have exactly one solution,
