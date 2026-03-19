039151259f3bfb912f5732d65f9a1fbeb58d6f53
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-03-19 09:38:26 +0530

    Update Day60_FindAllNumbersDisappearedinanArray.cpp

--- Day60_FindAllNumbersDisappearedinanArray.cpp
+++ Day60_FindAllNumbersDisappearedinanArray.cpp
@@ -1,35 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution
 {
 public:
-    vector<int> findDisappearedNumbers(vector<int> &nums)
-    {
+    vector<int> findDisappearedNumbers(vector<int> &nums) {
         int i = 0;
         int n = nums.size();
 
-        // 🔁 Cyclic Sort: place each number at correct index (value - 1)
-        while (i < n)
-        {
+        while (i < n){
             int correctIndex = nums[i] - 1;
 
-            if (nums[i] != nums[correctIndex])
-            {
+            if (nums[i] != nums[correctIndex]){
                 swap(nums[i], nums[correctIndex]);
-            }
-            else
-            {
+            } else {
                 i++;
             }
         }
 
-        // 📌 Collect missing numbers
         vector<int> result;
-        for (int i = 0; i < n; i++)
-        {
-            if (nums[i] != i + 1)
-            {
+        for (int i = 0; i < n; i++){
+            if (nums[i] != i + 1){
                 result.push_back(i + 1);
             }
         }
@@ -38,17 +30,14 @@ public:
     }
 };
 
-int main()
-{
+int main(){
     Solution sol;
 
     vector<int> nums = {4, 3, 2, 7, 8, 2, 3, 1};
-
     vector<int> result = sol.findDisappearedNumbers(nums);
 
     cout << "Missing numbers: ";
-    for (int num : result)
-    {
+    for (int num : result){
         cout << num << " ";
     }
 
