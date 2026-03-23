b3bb1b5c85aa3cd086d7e6ca16b789e6a5530bb1
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-03-23 22:49:39 +0530

    Update Day66_ClimbingStairs.cpp

--- Day66_ClimbingStairs.cpp
+++ Day66_ClimbingStairs.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
 
+int climbingStairs(int n)
+{
+    if (n <= 2)
+        return n;
+
+    int prev2 = 1;
+    int prev1 = 2;
+
+    for (int i = 3; i <= n; i++)
+    {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Answer: " << climbingStairs(n);
+    return 0;
+}
