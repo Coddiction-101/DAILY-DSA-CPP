8efe0ce12dee5e863c916c0633a65e35632b155d
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-03-23 22:59:18 +0530

    Update Day64_AppendCharacterstoStringtoMakeSubsequence.cpp

--- Day64_AppendCharacterstoStringtoMakeSubsequence.cpp
+++ Day64_AppendCharacterstoStringtoMakeSubsequence.cpp
@@ -1,1 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+class Solution
+{
+public:
+    int appendCharacters(string s, string t)
+    {
+        int i = 0, j = 0;
+
+        while (i < s.size() && j < t.size())
+        {
+            if (s[i] == t[j])
+            {
+                j++;
+            }
+            i++;
+        }
+
+        return t.size() - j;
+    }
+};
+
+int main()
+{
+    Solution obj;
+
+    cout << obj.appendCharacters("coaching", "coding") << endl;
+    cout << obj.appendCharacters("abcde", "a") << endl;
+    cout << obj.appendCharacters("z", "abcde") << endl;
+    return 0;
+}
