f1f0f9296238dd81dd51a71640a1c39f76faca44
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-03-23 22:23:32 +0530

    Update Day65_largestNumber.cpp

--- Day65_largestNumber.cpp
+++ Day65_largestNumber.cpp
@@ -1,1 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+class Solution
+{
+public:
+    static bool cmp(string a, string b)
+    {
+        return a + b > b + a;
+    }
+
+    string largestNumber(vector<int> &nums)
+    {
+
+        vector<string> arr;
+        for (
+            int num : nums)
+        {
+            arr.push_back(to_string(num));
+        }
+
+        sort(arr.begin(), arr.end(), cmp);
+
+        if (arr[0] == "0")
+            return "0";
+
+        string result = "";
+        for (string s : arr)
+        {
+            result += s;
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    Solution obj;
+
+    vector<int> nums = {3, 30, 34, 5, 9};
+
+    cout << obj.largestNumber(nums) << endl;
+    return 0;
+}
