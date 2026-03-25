8c2e770a51134b2678dccf21f13cbc5fcf6b5f5b
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-03-25 20:27:51 +0530

    Update Day67_PalindromeLinkedList.cpp

--- Day67_PalindromeLinkedList.cpp
+++ Day67_PalindromeLinkedList.cpp
@@ -1,1 +1,63 @@
+#include <iostream>
+using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) return true;
+
+        // find middle
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // reverse second half
+        ListNode* prev = NULL;
+        ListNode* curr = slow;
+
+        while (curr) {
+            ListNode* nextTemp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        // compare both halves
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while (second) {
+            if (first->val != second->val) return false;
+            first = first->next;
+            second = second->next;
+        }
+
+        return true;
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(2);
+    head->next->next->next = new ListNode(1);
+
+    Solution obj;
+
+    if (obj.isPalindrome(head))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
