8e7308a8a0034c708e29c06448fdb604a99b95db
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-03-31 22:01:22 +0530

    Update Day73_RandomNote.cpp

--- Day73_RandomNote.cpp
+++ Day73_RandomNote.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int freq[26] = {0};
+
+        for (char c : magazine)
+            freq[c - 'a']++;
+
+        for (char c : ransomNote) {
+            if (freq[c - 'a'] == 0)
+                return false;
+            freq[c - 'a']--;
+        }
+
+        return true;
+    }
+};
+
+int main() {
+    Solution obj;
+
+    string ransomNote = "aa";
+    string magazine = "aab";
+
+    cout << (obj.canConstruct(ransomNote, magazine) ? "True" : "False");
+
+    return 0;
+}
