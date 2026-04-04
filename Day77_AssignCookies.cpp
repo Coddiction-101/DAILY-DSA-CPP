9b257edfc185c74482e1b61664080b8568a27858
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-04-04 20:52:03 +0530

    Update Day77_AssignCookies.cpp

--- Day77_AssignCookies.cpp
+++ Day77_AssignCookies.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int assignCookies(vector<int> &g, vector<int> &s)
+{
+    sort(g.begin(), g.end());
+    sort(s.begin(), s.end());
+
+    int i = 0;
+    int j = 0;
+
+    while (i < g.size() && j < s.size())
+    {
+        if (s[j] >= g[i])
+        {
+            i++;
+            j++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+    return i;
+}
+
+int main()
+{
+    vector<int> g = {1, 2, 3};
+    vector<int> s = {1, 1};
+
+    int result = assignCookies(g, s);
+
+    cout << "Maximum satisfied children: " << result << endl;
+    return 0;
+}
