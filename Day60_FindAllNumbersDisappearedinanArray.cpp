f2c5e5bf1c0db9994a632caca231df7bfd22e268
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-06-08 22:23:45 +0530

    Update Day60_FindAllNumbersDisappearedinanArray.cpp

--- Day60_FindAllNumbersDisappearedinanArray.cpp
+++ Day60_FindAllNumbersDisappearedinanArray.cpp
@@ -1,18 +1,13 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-class Solution
-{
+class Solution {
 public:
-    vector<int> findDisappearedNumbers(vector<int> &nums) {
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
         int i = 0;
         int n = nums.size();
 
-        while (i < n){
+        while (i < n) {
             int correctIndex = nums[i] - 1;
 
-            if (nums[i] != nums[correctIndex]){
+            if (nums[i] != nums[correctIndex]) {
                 swap(nums[i], nums[correctIndex]);
             } else {
                 i++;
@@ -20,8 +15,9 @@ public:
         }
 
         vector<int> result;
-        for (int i = 0; i < n; i++){
-            if (nums[i] != i + 1){
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
                 result.push_back(i + 1);
             }
         }
@@ -29,17 +25,3 @@ public:
         return result;
     }
 };
-
-int main(){
-    Solution sol;
-
-    vector<int> nums = {4, 3, 2, 7, 8, 2, 3, 1};
-    vector<int> result = sol.findDisappearedNumbers(nums);
-
-    cout << "Missing numbers: ";
-    for (int num : result){
-        cout << num << " ";
-    }
-
-    return 0;
-}
