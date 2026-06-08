c8db0028160c610dcfd4e46bd26997e844e70c75
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-06-08 22:27:12 +0530

    Update Day63_NumberofSubsequencesThatSatisfytheGivenSumCondition.cpp

--- Day63_NumberofSubsequencesThatSatisfytheGivenSumCondition.cpp
+++ Day63_NumberofSubsequencesThatSatisfytheGivenSumCondition.cpp
@@ -1,6 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
@@ -9,11 +6,12 @@ public:
         sort(nums.begin(), nums.end());
 
         int n = nums.size();
-        int left = 0, right = n - 1;
+        int left = 0;
+        int right = n - 1;
 
-        // powers of 2
         vector<int> power(n);
         power[0] = 1;
+
         for (int i = 1; i < n; i++) {
             power[i] = (power[i - 1] * 2) % MOD;
         }
@@ -21,12 +19,10 @@ public:
         int count = 0;
 
         while (left <= right) {
-
             if (nums[left] + nums[right] <= target) {
                 count = (count + power[right - left]) % MOD;
                 left++;
-            }
-            else {
+            } else {
                 right--;
             }
         }
@@ -34,14 +30,3 @@ public:
         return count;
     }
 };
-
-int main() {
-    Solution obj;
-
-    vector<int> nums = {3, 5, 6, 7};
-    int target = 9;
-
-    cout << obj.numSubseq(nums, target) << endl;
-
-    return 0;
-}
