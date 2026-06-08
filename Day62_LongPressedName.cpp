6822757a2e3f281a90e2d065ff56a70560699bb3
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-06-08 22:25:52 +0530

    Update Day62_LongPressedName.cpp

--- Day62_LongPressedName.cpp
+++ Day62_LongPressedName.cpp
@@ -1,24 +1,17 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
     bool isLongPressedName(string name, string typed) {
-        int i = 0, j = 0;
+        int i = 0;
+        int j = 0;
 
         while (j < typed.size()) {
-
             if (i < name.size() && name[i] == typed[j]) {
-                i++; 
+                i++;
                 j++;
             }
-
-     
             else if (j > 0 && typed[j] == typed[j - 1]) {
                 j++;
             }
-
             else {
                 return false;
             }
@@ -27,14 +20,3 @@ public:
         return i == name.size();
     }
 };
-
-int main() {
-    Solution obj;
-
-    // Sample test cases
-    cout << obj.isLongPressedName("alex", "aaleex") << endl;  
-    cout << obj.isLongPressedName("saeed", "ssaaedd") << endl; 
-    cout << obj.isLongPressedName("leelee", "lleeelee") << endl; 
-
-    return 0;
-}
