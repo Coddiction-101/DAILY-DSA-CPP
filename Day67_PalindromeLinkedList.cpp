e8d73007e70bb53dc4b51cc93756476c5af5af87
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-06-20 13:33:28 +0530

    Update Day67_PalindromeLinkedList.cpp

--- Day67_PalindromeLinkedList.cpp
+++ Day67_PalindromeLinkedList.cpp
@@ -1,43 +1,38 @@
-#include <iostream>
-using namespace std;
-
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(ListNode* head) {
-        if (!head || !head->next) return true;
+    bool isPalindrome(ListNode* head)
+    {
+        if (!head || !head->next)
+            return true;
 
-        // find middle
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while (fast && fast->next) {
+        while (fast && fast->next)
+        {
             slow = slow->next;
             fast = fast->next->next;
         }
 
-        // reverse second half
-        ListNode* prev = NULL;
-        ListNode* curr = slow;
+        ListNode* prev = nullptr;
 
-        while (curr) {
-            ListNode* nextTemp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextTemp;
+        while (slow)
+        {
+            ListNode* next = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = next;
         }
 
-        // compare both halves
         ListNode* first = head;
         ListNode* second = prev;
 
-        while (second) {
-            if (first->val != second->val) return false;
+        while (second)
+        {
+            if (first->val != second->val)
+                return false;
+
             first = first->next;
             second = second->next;
         }
@@ -45,19 +40,3 @@ public:
         return true;
     }
 };
-
-int main() {
-    ListNode* head = new ListNode(1);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(2);
-    head->next->next->next = new ListNode(1);
-
-    Solution obj;
-
-    if (obj.isPalindrome(head))
-        cout << "true";
-    else
-        cout << "false";
-
-    return 0;
-}
