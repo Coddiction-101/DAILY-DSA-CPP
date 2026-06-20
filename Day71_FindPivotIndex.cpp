f35007fcf3d3505804b5f59c730bf00b21f8845a
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-06-20 13:37:51 +0530

    Update Day71_FindPivotIndex.cpp

--- Day71_FindPivotIndex.cpp
+++ Day71_FindPivotIndex.cpp
@@ -1,26 +1,21 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-class Solution {
+class Solution
+{
 public:
-    int pivotIndex(vector<int>& nums) {
+    int pivotIndex(vector<int>& nums)
+    {
         int totalSum = 0;
 
-        // Calculate total sum
-        for(int num : nums) {
+        for (int num : nums)
             totalSum += num;
-        }
 
         int leftSum = 0;
 
-        // Find pivot index
-        for(int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++)
+        {
             int rightSum = totalSum - leftSum - nums[i];
 
-            if(leftSum == rightSum) {
+            if (leftSum == rightSum)
                 return i;
-            }
 
             leftSum += nums[i];
         }
@@ -28,19 +23,3 @@ public:
         return -1;
     }
 };
-
-int main() {
-    Solution obj;
-
-    vector<int> nums = {1, 7, 3, 6, 5, 6};
-
-    int result = obj.pivotIndex(nums);
-
-    if(result != -1) {
-        cout << "Pivot Index: " << result << endl;
-    } else {
-        cout << "No Pivot Index Found" << endl;
-    }
-
-    return 0;
-}
