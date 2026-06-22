05496506937d01cb3dd7022e578a1f99beb7d390
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-06-22 20:44:38 +0530

    Update Day77_AssignCookies.cpp

--- Day77_AssignCookies.cpp
+++ Day77_AssignCookies.cpp
@@ -1,38 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
 
-int assignCookies(vector<int> &g, vector<int> &s)
-{
-    sort(g.begin(), g.end());
-    sort(s.begin(), s.end());
+        int i = 0;
+        int j = 0;
 
-    int i = 0;
-    int j = 0;
-
-    while (i < g.size() && j < s.size())
-    {
-        if (s[j] >= g[i])
-        {
-            i++;
-            j++;
+        while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
         }
-        else
-        {
-            j++;
-        }
+
+        return i;
     }
-    return i;
-}
-
-int main()
-{
-    vector<int> g = {1, 2, 3};
-    vector<int> s = {1, 1};
-
-    int result = assignCookies(g, s);
-
-    cout << "Maximum satisfied children: " << result << endl;
-    return 0;
-}
+};
