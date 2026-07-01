ba418c313a4373559f7e6372de4b79c01dac3d89
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-07-01 22:39:43 +0530

    Update Day82_RotateList.cpp

--- Day82_RotateList.cpp
+++ Day82_RotateList.cpp
@@ -1,1 +1,28 @@
+ class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head || k == 0) return head;
 
+        ListNode* temp = head;
+        int length = 1;
+        while (temp->next) {
+            temp = temp->next;
+            length++;
+        }
+
+        temp->next = head;  
+
+        k = k % length;
+        int stepsToNewTail = length - k - 1;
+        ListNode* newTail = head;
+
+        for (int i = 0; i < stepsToNewTail; i++) {
+            newTail = newTail->next;
+        }
+
+        ListNode* newHead = newTail->next;
+        newTail->next = nullptr;
+
+        return newHead;
+    }
+};
