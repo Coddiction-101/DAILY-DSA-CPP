b29ecbaad9d60124d0a9bd93b5c0c0f3e304529b
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-07-07 21:32:21 +0530

    Update Day87_DayValidPalindromeII.cpp

--- Day87_DayValidPalindromeII.cpp
+++ Day87_DayValidPalindromeII.cpp
@@ -1,1 +1,39 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
 
+        while (left < right) {
+            if (s[left] != s[right]) {
+
+                int i = left + 1;
+                int j = right;
+
+                while (i < j && s[i] == s[j]) {
+                    i++;
+                    j--;
+                }
+
+                if (i >= j) {
+                    return true;
+                }
+
+                i = left;
+                j = right - 1;
+
+                while (i < j && s[i] == s[j]) {
+                    i++;
+                    j--;
+                }
+
+                return i >= j;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
